ac1fd13f1bede22cf7579d8fd2588eabef48a314
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-02-10 12:10:04 +0100

    first commit

---
+++ basic.py
@@ -0,0 +1,7 @@
+from langchain.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+llm = Ollama(model="llama2", callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
+
+llm("Tell me 6 facts about the sun:")
