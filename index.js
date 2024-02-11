322338caf1f1faefff5acc4edb3d7221c5005961
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-02-11 23:52:38 +0100

    feat: use mongodb instead of supabase
    switch to mongodb (version 6.3.0) to store embedding vectors.
    Note that mongo's latest version could have some compatibility issues with other dependencies.

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-import fs from "fs/promises";
+import { MongoDBAtlasVectorSearch } from "@langchain/community/vectorstores/mongodb_atlas";
+import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+dotenv.config();
 
-try {
-  const text = await fs.readFile("./ricettario.txt", "utf-8");
+const client = new MongoClient(process.env.MONGODB_ATLAS_URI || "");
 
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    separators: ["\n\n", "\n", " ", "  "],
-    chunkOverlap: 50,
-  });
+const namespace = "langchain.test";
 
-  const output = await splitter.createDocuments([text]);
+const [dbName, collectionName] = namespace.split(".");
 
-  console.log(output);
-} catch (err) {
-  console.log(err);
-}
+const collection = client.db(dbName).collection(collectionName);
+
+const vectorstore = await MongoDBAtlasVectorSearch.fromTexts(
+  ["Hello world", "Bye bye", "What's this?"],
+
+  [{ id: 2 }, { id: 1 }, { id: 3 }],
+
+  new OllamaEmbeddings(),
+
+  {
+    collection,
+    indexName: "default", // The name of the Atlas search index. Defaults to "default"
+    textKey: "text", // The name of the collection field containing the raw content. Defaults to "text"
+    embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+  },
+);
+
+const assignedIds = await vectorstore.addDocuments([
+  { pageContent: "upsertable", metadata: {} },
+]);
+
+const upsertedDocs = [{ pageContent: "overwritten", metadata: {} }];
+
+await vectorstore.addDocuments(upsertedDocs, { ids: assignedIds });
+
+await client.close();
+
+console.log("Done");
