3657c42d08141a40bbf8e6f4eae407f3f9ece8c0
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-02-13 13:12:21 +0100

    fix: solve embedding issue
    The issue was not caused by the code itself, rather by an ollama
    service that was still running and prevented the model to work.

--- index.js
+++ index.js
@@ -8,59 +8,35 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 dotenv.config();
 
 const text = fs.readFileSync("ricettario.txt", "utf-8");
-
 const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 500,
   separators: ["\n\n", "\n", " ", ""],
   chunkOverlap: 50,
 });
 
+console.log("Splitting text document into chunks...");
+console.time("Text splitting completed in");
 const output = await splitter.createDocuments([text]);
-
-console.log(output);
+console.timeEnd("Text splitting completed in");
 
 const client = new MongoClient(process.env.MONGODB_ATLAS_URI || "");
-
 const namespace = "langchain.test";
-
 const [dbName, collectionName] = namespace.split(".");
-
 const collection = client.db(dbName).collection(collectionName);
 
-const embeddings = new OllamaEmbeddings({ model: "llama2" });
-
-const vectorstore = new MongoDBAtlasVectorSearch(
-  embeddings,
-
+console.log("Inserting documents into MongoDB Atlas collection...");
+console.time("Document insertion completed in");
+/* await MongoDBAtlasVectorSearch.fromDocuments(
+  output,
+  new OllamaEmbeddings(),
   {
     collection,
     indexName: "default", // The name of the Atlas search index. Defaults to "default"
     textKey: "text", // The name of the collection field containing the raw content. Defaults to "text"
     embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
   },
-);
-
-const transformedOutput = await Promise.all(
-  output.map(async (doc) => {
-    console.log(`Embedding document`);
-
-    const embedding = await embeddings.embedDocuments([doc.pageContent]);
-
-    console.log(`Document embedded`);
-
-    return {
-      pageContent: doc.pageContent,
-
-      embedding: embedding,
-
-      metadata: {},
-    };
-  }),
-);
-
-console.log(transformedOutput);
-
-await vectorstore.addDocuments(transformedOutput);
+); */
+console.timeEnd("Document insertion completed in");
 
 await client.close();
 
