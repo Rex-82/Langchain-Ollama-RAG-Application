7369c65ebc0541154f6d278724d967e1b2791905
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-02-16 15:34:32 +0100

    feat: add chatbot related code
    Added code to generate a standalone question given an user question.
    This will be used to search for matches in the vector storage.

--- page/index.js
+++ page/index.js
@@ -1,5 +1,33 @@
-// document.addEventListener("DOMContentLoaded", () => {});
+// ----------------- Chatbot related code -------------------
 
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOllama } from "@langchain/community/chat_models/ollama";
+
+export async function generateStandaloneQuestion() {
+  const llm = new ChatOllama({
+    model: "llama2",
+  });
+
+  const standaloneQuestionTemplate =
+    "Given a question, convert it to a standalone question. question: {question} standalone question:";
+
+  const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
+    standaloneQuestionTemplate,
+  );
+
+  const standaloneQuestionChain = standaloneQuestionPrompt.pipe(llm);
+
+  const response = await standaloneQuestionChain.invoke({
+    question:
+      "What are some good february dishes? I would like to make something for my family",
+  });
+
+  console.log(response);
+}
+// ---------------- Webpage related code -------------------
+
+// document.addEventListener("DOMContentLoaded", () => {});
+/*
 document.addEventListener("submit", (e) => {
   e.preventDefault();
   progressConversation();
@@ -8,12 +36,12 @@ document.addEventListener("submit", (e) => {
 async function progressConversation() {
   const userInput = document.getElementById("card-input");
   const chatbotConversation = document.getElementById(
-    "chatbot-conversation-container"
+    "chatbot-conversation-container",
   );
   const question = userInput.value.trim(); // Trim whitespace from input
 
   if (!question) {
-    // Focus on the input area
+    // Focus on the input area if textArea is empty
     userInput.focus();
   } else {
     userInput.value = "";
@@ -25,8 +53,8 @@ async function progressConversation() {
     newHumanSpeechBubble.textContent = question;
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
 
-    // Add AI message (You need to define `result` somewhere)
-    const result = "AI response"; // Example response, replace with your actual response
+    // Add AI message
+    const result = "AI response";
     const newAiSpeechBubble = document.createElement("div");
     newAiSpeechBubble.classList.add("speech", "speech-ai", "row");
     chatbotConversation.appendChild(newAiSpeechBubble);
@@ -34,3 +62,4 @@ async function progressConversation() {
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
   }
 }
+*/
