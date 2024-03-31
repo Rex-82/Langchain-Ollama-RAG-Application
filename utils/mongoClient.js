d70da6638d205bb1b5d0abbbcdd877bd4678f878
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-04-01 00:21:18 +0200

    feat: add jsdoc and small improvements
    Added jsdoc to MongoDB related utility code and made some error handling improvements

--- utils/mongoClient.js
+++ utils/mongoClient.js
@@ -1,17 +1,22 @@
-import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
 dotenv.config();
 
-let client = null;
+/**@type {MongoClient|null}*/ let client = null;
 
-// Connect to the MongoDB cluster and return the MongoDB client
-// ( If the client is already connected, return the existing client )
+/**
+ * Connects to the MongoDB cluster and returns the MongoDB client object
+ * ( If the client is already connected, returns the existing client )
+ *
+ * @async
+ * @returns {Promise<MongoClient>} Returns the client object
+ * @throws {Error} - Throws an error if cannot connect or ping DB
+ */
 export async function connectMongoClient() {
   try {
     if (!client) {
       client = new MongoClient(process.env.MONGODB_ATLAS_URI || "");
-
       // Connect the client to the server (optional starting in v4.7)
       await client.connect();
 
@@ -24,12 +29,17 @@ export async function connectMongoClient() {
     }
     return client;
   } catch (e) {
-    console.error(e);
-    return e;
+    console.error("Error while connecting to DB:", e);
+    throw e;
   }
 }
 
-// Close the connection to the MongoDB cluster
+/**
+ * Closes the connection to the MongoDB Cluster
+ *
+ * @async
+ * @returns {Promise<void>} 
+ */
 export async function disconnectMongoClient() {
   if (client) {
     await client.close();
--- utils/retriever.js
+++ utils/retriever.js
@@ -1,37 +1,59 @@
-import { MongoDBAtlasVectorSearch } from "@langchain/community/vectorstores/mongodb_atlas";
 import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/hf_transformers";
+import { MongoDBAtlasVectorSearch } from "@langchain/community/vectorstores/mongodb_atlas";
+import { VectorStoreRetriever } from "@langchain/core/vectorstores";
 import dotenv from "dotenv";
+import { Collection, MongoClient } from "mongodb";
 import { connectMongoClient } from "./mongoClient.js";
 
 dotenv.config();
 
-let retriever;
 
-try {
-  const client = await connectMongoClient();
-  const dbName = process.env.MONGODB_DB_NAME;
-  const collectionName = process.env.MONGODB_COLLECTION_NAME;
-  const collection = client.db(dbName).collection(collectionName);
+/**
+ * Retrieves documents from DB conllection to be used in chain
+ *
+ * @async
+ * @throws {Error} - Throws an error if either one of the env variables is not set
+ * @throws {Error} - Throws an error if cannot retrieve document embeddings from DB
+ * @returns {Promise<VectorStoreRetriever>} Returns vectorized documents
+ */
+export default async function retrieve() {
+  try {
+    /** @type {MongoClient}*/
+    const client = await connectMongoClient();
 
-  console.log("Retrieving documents from MongoDB Atlas collection...");
-  console.time("Document retrieve completed in");
-  const vectorStore = new MongoDBAtlasVectorSearch(
-    new HuggingFaceTransformersEmbeddings({
-      modelName: process.env.EMBEDDING_HF_MODEL_NAME,
-    }),
-    {
-      collection,
-      indexName: "default", // The name of the Atlas search index. Defaults to "default"
-      textKey: "text", // The name of the collection field containing the raw content. Defaults to "text"
-      embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
-    },
-  );
+    /** @type {string|undefined}*/
+    const dbName = process.env.MONGODB_DB_NAME;
+    /** @type {string|undefined}*/
+    const collectionName = process.env.MONGODB_COLLECTION_NAME;
 
-  retriever = vectorStore.asRetriever();
+    if (!dbName || !collectionName) {
+      throw new Error("Error while trying to retrieve documents: DB Env variables not set");
+    } else {
+      /** @type {Collection}*/
+      const collection = client.db(dbName).collection(collectionName);
 
-  console.timeEnd("Document retrieve completed in");
-} catch (e) {
-  console.error(e);
+      console.log("Retrieving documents from MongoDB Atlas collection...");
+      console.time("Document retrieve completed in");
+      /** @type {MongoDBAtlasVectorSearch}*/
+      const vectorStore = new MongoDBAtlasVectorSearch(
+        new HuggingFaceTransformersEmbeddings({
+          modelName: process.env.EMBEDDING_HF_MODEL_NAME,
+        }),
+        {
+          collection,
+          indexName: "default", // The name of the Atlas search index. Defaults to "default"
+          textKey: "text", // The name of the collection field containing the raw content. Defaults to "text"
+          embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+        },
+      );
+
+      console.timeEnd("Document retrieve completed in");
+      return vectorStore.asRetriever();
+
+    }
+  } catch (e) {
+    console.error("Error while retrieving documents from DB", e);
+    throw e;
+  }
+
 }
-
-export { retriever };
