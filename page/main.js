277bcd2e10267436ae1d3060bdd8e0b50b73a64b
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-04-02 11:07:36 +0200

    chore: rename main file and add defer
    Changed name for page/index.js in page/main.js to prevent ambiguity
    with the other index.js file.
    Changed script mode in index.html to be deferred to comply with
    modern standards and to improve page loading times.

--- page/main.js
+++ page/main.js
@@ -1,50 +1,80 @@
 // ----------------- Chatbot related code -------------------
 
-import { PromptTemplate } from "@langchain/core/prompts";
-import { StringOutputParser } from "@langchain/core/output_parsers";
-import {
-  RunnableSequence,
-  RunnablePassthrough,
-} from "@langchain/core/runnables";
 import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import dotenv from "dotenv";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
+import {
+  RunnablePassthrough,
+  RunnableSequence,
+} from "@langchain/core/runnables";
 import { env as xenova } from "@xenova/transformers";
-import { retriever } from "../utils/retriever.js";
+import dotenv from "dotenv";
 import { combineDocuments } from "../utils/combineDocuments.js";
+import retriever from "../utils/retriever.js";
 
+//----- Xenova transformer settings to use local models with HuggingFace instead of remote ones -----
 // Specify a custom location for models (defaults to '/models/').
 xenova.localModelPath = "./models/";
 
 // Disable the loading of remote models from the Hugging Face Hub:
 xenova.allowRemoteModels = false;
 
+// Setting env file location
 dotenv.config();
 
+// Main exported function:
+// 1. Starts by setting the model to use (ChatOllama in this case with a model set in .env)
+// 2. Declares the standalone question template with a custom {question} field.
+//    This field will contain the user input
+// 3. Declares the answer template with {context} and {question} (RAG)
+// 4. Sets up the question chain using RunnableSequence (pipe could also have been used here)
+// 5. Sets up the retriever chain to get a list of objects matching standalone_question
+// 6. Sets up the answer chain (Similar as the question chain)
+// 7. Combines the 3 chain pieces in a RunnableSequence passing values accordingly
+// 8. Invokes the chain by passing user's question
+// 9. Logs out the response (answers the question)
+
 export async function generateStandaloneQuestion() {
+  // Creates the chat model to use (set in the .env file)
   const llm = new ChatOllama({
     model: process.env.CHAT_OLLAMA_MODEL_NAME,
   });
 
+  // Standalone question template
+  // This is used to tell the model to generate a template given user's question
   const standaloneQuestionTemplate =
     "Given a question, convert it to a standalone question. question: {question} standalone question:";
 
+  // Defines question template with variables ({question} in this case)
+  // Note that variables can be specified or inferred
+  // For more info: https://js.langchain.com/docs/modules/model_io/prompts/quick_start#create-a-prompt-template
   const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
     standaloneQuestionTemplate,
   );
 
-  const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say \"I'm sorry, I don't know the answer to that.\" And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+  // Answer template
+  // Similarly to the question template. This is used to set the model persona and pass context and question
+  const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
 context: {context}
 question: {question}
 answer:`;
 
+  // Defines answer template with variables
   const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
 
+  console.timeEnd("Template question generation")
+
+  // Question chain piece
+  // Parses the standalone question as a string
+  // For more info on StringOutputParser: https://js.langchain.com/docs/modules/model_io/output_parsers/types/string
   const standaloneQuestionChain = RunnableSequence.from([
     standaloneQuestionPrompt,
     llm,
     new StringOutputParser(),
   ]);
 
+  // Retriever chain piece
+  // Takes the question string from the previous chain piece and retrieves matching objects
   const retrieverChain = RunnableSequence.from([
     // Takes the previous result (from standalone question's chain)
     // and pass only the standalone_question string from the object
@@ -56,19 +86,23 @@ answer:`;
     combineDocuments,
   ]);
 
+  // Answer chain piece
+  // Parses the answer as a string
   const answerChain = RunnableSequence.from([
     answerPrompt,
     llm,
     new StringOutputParser(),
   ]);
 
+  // Complete chain
+  // Generates the answer given a question
   const chain = RunnableSequence.from([
     {
-      standalone_question: standaloneQuestionChain,
-      original_input: new RunnablePassthrough(),
+      standalone_question: standaloneQuestionChain, // standaloone_question contains the result from the first chain piece
+      original_input: new RunnablePassthrough(), // original_input contains the user's question that will be passed to the answer chain piece
     },
     {
-      context: retrieverChain,
+      context: retrieverChain, //
       question: ({ original_input }) => original_input.question,
     },
     answerChain,
