ced94f783932469a9dc51db0a72a82c2bedd4093
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-04-02 11:10:47 +0200

    chore: comment unused code
    Commented out code used to vectorize document

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { MongoDBAtlasVectorSearch } from "@langchain/community/vectorstores/mongodb_atlas";
-import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/hf_transformers";
+// import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/hf_transformers";
+// import { MongoDBAtlasVectorSearch } from "@langchain/community/vectorstores/mongodb_atlas";
+import { env } from "@xenova/transformers";
 import dotenv from "dotenv";
-import fs from "fs";
+// import fs from "fs";
+// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { generateStandaloneQuestion } from "./page/main.js";
 import {
-  connectMongoClient,
+  // connectMongoClient,
   disconnectMongoClient,
 } from "./utils/mongoClient.js";
-import { generateStandaloneQuestion } from "./page/index.js";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { env } from "@xenova/transformers";
 
 // Specify a custom location for models (defaults to '/models/').
 env.localModelPath = "./models/";
@@ -17,46 +17,57 @@ env.allowRemoteModels = false;
 
 dotenv.config();
 
-const text = fs.readFileSync(process.env.DOCUMENT, "utf-8");
-const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 500,
-  separators: ["\n\n", "\n", " ", ""],
-  chunkOverlap: 50,
-});
+// const text = fs.readFileSync(process.env.DOCUMENT, "utf-8");
+// const splitter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 500,
+//   separators: ["\n\n", "\n", " ", ""],
+//   chunkOverlap: 50,
+// });
 
-console.log("Splitting text document into chunks...");
-console.time("Text splitting completed in");
-const output = await splitter.createDocuments([text]);
-console.timeEnd("Text splitting completed in");
+/**
+ * Main to wrap code logic
+ *
+ * @async
+ * @returns {Promise<void>} Executes the code and returns nothing
+ */
+async function main() {
+  console.log("Splitting text document into chunks...");
+  console.time("Text splitting completed in");
+  // const output = await splitter.createDocuments([text]);
+  console.timeEnd("Text splitting completed in");
 
-const client = await connectMongoClient();
-const dbName = process.env.MONGODB_DB_NAME;
-const collectionName = process.env.MONGODB_COLLECTION_NAME;
-const collection = client.db(dbName).collection(collectionName);
+  // const client = await connectMongoClient();
+  // const dbName = process.env.MONGODB_DB_NAME;
+  // const collectionName = process.env.MONGODB_COLLECTION_NAME;
+  // const collection = client.db(dbName).collection(collectionName);
 
-console.log("Inserting documents into MongoDB Atlas collection...");
-console.time("Document insertion completed in");
-/* await MongoDBAtlasVectorSearch.fromDocuments(
-  output,
-  new HuggingFaceTransformersEmbeddings({
-    modelName: process.env.EMBEDDING_HF_MODEL_NAME,
-  }),
-  {
-    collection,
-    indexName: "default", // The name of the Atlas search index. Defaults to "default"
-    textKey: "text", // The name of the collection field containing the raw content. Defaults to "text"
-    embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
-  },
-); */
-console.timeEnd("Document insertion completed in");
+  console.log("Inserting documents into MongoDB Atlas collection...");
+  console.time("Document insertion completed in");
+  /* await MongoDBAtlasVectorSearch.fromDocuments(
+    output,
+    new HuggingFaceTransformersEmbeddings({
+      modelName: process.env.EMBEDDING_HF_MODEL_NAME,
+    }),
+    {
+      collection,
+      indexName: "default", // The name of the Atlas search index. Defaults to "default"
+      textKey: "text", // The name of the collection field containing the raw content. Defaults to "text"
+      embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+    },
+  ); */
+  console.timeEnd("Document insertion completed in");
 
-console.log("Running template question generation...");
+  console.log("Running template question generation...");
 
-console.time("Completed in");
+  console.time("Completed in");
 
-await generateStandaloneQuestion();
+  await generateStandaloneQuestion();
 
-console.timeEnd("Completed in");
+  console.timeEnd("Completed in");
 
-await disconnectMongoClient();
-console.log("Done");
+  await disconnectMongoClient();
+  console.log("Done");
+}
+
+
+main();
